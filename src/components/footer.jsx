761fad0ad4a0c889c726845fe2866dbd61c4dcfe
daphane taylor <daphane.elizabeth@gmail.com>
2024-12-21 18:07:35 -0500

    class 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import './styles/footer.css'
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <div className="footer-section">
+                    <p>Created By: Daphane Taylor</p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
